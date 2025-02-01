ff3af3f73978d351b191a5590315a222ce51e6dd
Benjamin Massey <benjamin.w.massey@gmail.com>
2025-02-01 00:30:11 -0800

    added a path to the main scene and a script for food to move along it

---
+++ scripts/food.gd
@@ -0,0 +1,20 @@
+extends Node3D;
+
+@export var speed: float = 1.0;
+
+var current_path = 0;
+
+func _process(delta):
+	move_path(delta);
+
+func move_path(delta):
+	if %path.get_child_count() < current_path:
+		return;
+	var target = %path.get_child(current_path);
+	if target == null:
+		return;
+	var target_position = target.global_position;
+	var direction = (target_position - global_position).normalized();
+	global_position += direction * speed * delta
+	if global_position.distance_to(target_position) < 0.1:
+		current_path += 1;
